c3a19eddc87072e8f677aeba95f958ff13305b82
Hüseyin <101873227+huseyin-aln@users.noreply.github.com>
2023-01-09 15:24:19 +0300

    added login page

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,6 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Footer, Header } from "../components";
-import { Home, Contact, Cart, Admin, OrderHistory } from "../pages";
+import {
+  Home,
+  Contact,
+  Cart,
+  Admin,
+  OrderHistory,
+  Login,
+  Register,
+  Reset,
+} from "../pages";
 
 const AppRouter = () => {
   return (
@@ -13,6 +22,9 @@ const AppRouter = () => {
           <Route path="/cart" element={<Cart />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/orderhistory" element={<OrderHistory />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/reset" element={<Reset />} />
         </Routes>
         <Footer />
       </BrowserRouter>
