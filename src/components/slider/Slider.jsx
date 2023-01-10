dfe3c230a8e563a49964c12401ada7589c59622c
Hüseyin <101873227+huseyin-aln@users.noreply.github.com>
2023-01-10 15:23:08 +0300

    added arrow

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -6,10 +6,20 @@ import "./Slider.scss";
 const Slider = () => {
   const [currentSlide, setCurrentSlide] = useState(0);
 
+  const slideLength = sliderData.length;
+
+  const prevSlide = () => {
+    setCurrentSlide(currentSlide === 0 ? slideLength - 1 : currentSlide - 1);
+  };
+
+  const nextSlide = () => {
+    setCurrentSlide(currentSlide === slideLength - 1 ? 0 : currentSlide + 1);
+  };
+
   return (
     <div className="slider">
-      <AiOutlineArrowLeft className="arrow prev" />
-      <AiOutlineArrowRight className="arrow next" />
+      <AiOutlineArrowLeft className="arrow prev" onClick={prevSlide} />
+      <AiOutlineArrowRight className="arrow next" onClick={nextSlide} />
 
       {sliderData.map((slide, index) => {
         const { image, heading, desc } = slide;
